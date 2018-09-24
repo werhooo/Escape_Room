5eabfd1a3582bb4fdfeb96ba5ff56f221c30cffd
werhooo <40298658+werhooo@users.noreply.github.com>
2018-09-24 19:33:38 +0200

    Create Solution

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Escape_room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verhóczki Márton, I2P második kör feladat - Escape_Room
+
+
+
+
+
+
+
+
+        }
+    }
+}
