5df0587147b398089880a17e667a4331982115a2
werhooo <40298658+werhooo@users.noreply.github.com>
2018-09-26 15:10:37 +0200

    e,d,k,ny
    észak, dél, kelet, nyugat methodok

--- Program.cs
+++ Program.cs
@@ -12,15 +12,43 @@ namespace Escape_room
     //Állandók tárolása
     class global
     {
-       public int currentPlace = -1; // 1 nappali, -1 fürdőszoba
+        Program program = new Program();
+       public int currentPlace = 1; // 1 nappali, -1 fürdőszoba
        public static void nappaliInfo()
        {
-            Console.WriteLine("A nappaliban vagy, tőled jobbra van észak. Előtted (nyugatra) van egy ajtó, tőled jobbra(észak) pedig egy szekrény.");
+            Program.writeFancy("A nappaliban vagy, tőled jobbra van észak. Előtted (nyugatra) van egy ajtó, tőled jobbra(észak) pedig egy szekrény.");
+            
        }
        public static void furdoszobaInfo()
        {
-            Console.WriteLine("A fürdőszobában vagy, a szobában van egy kád. A nappaliba vissza tudsz menni keletre");
+            Program.writeFancy("A fürdőszobában vagy, a szobában van egy kád. A nappaliba vissza tudsz menni keletre");
        }
+        public static void szekrenyInfo()
+        {
+            Program.writeFancy("Ez egy szekrény, ???");  // csicska vagy
+
+        }
+        public static void dobozInfo()
+        {
+            Program.writeFancy("Ez egy doboz, ???");  // csicska vagy
+        }
+        public static void kulcsInfo()
+        {
+            Program.writeFancy("Ez egy kulcs, ???"); // csicska vagy
+        }
+        public static void ajtóInfo()
+        {
+            Program.writeFancy("Ez egy ajtó,?????"); // csicska vagy
+        }
+        public static void ablakInfo()
+        {
+            Program.writeFancy("Ez egy ablak,?????"); // csicska vagy
+        }
+        public static void feszitovasInfo()
+        {
+            Program.writeFancy("Ez egy feszítővas,?????"); // csicska vagy
+        }
+
 
     }
 
@@ -41,17 +69,17 @@ namespace Escape_room
 
 
             welcomeScreen();
-            intro();
-            string[] baseStory = new string[]{ "Egy szobában térsz magadhoz", "Nem emlékszel semmire", "Mit teszel?" };
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine("Nyomj 'ENTER'-t hogy továbblépj");
-            Console.WriteLine("----------------------------------");
-            writeFancy(baseStory);
+            //intro();
+            string[] baseStory = new string[] { "Egy szobában térsz magadhoz", "Nem emlékszel semmire", "Mit teszel?" };
             
-           
+            writeFancy(baseStory);
+
             
 
-           
+
+
+
+
 
             //parancs bekérése, kiértékelése
             //ha a felhasználó véletlenül entert nyom, vagy rosszul ütni be a parancsot, hibaüzenetet kap. és megy tovább a loop
@@ -61,7 +89,7 @@ namespace Escape_room
                 string UI = Console.ReadLine();
                 if (UI == "")
                 {
-                   
+
                     uiError = true;
                 }
                 if (uiError)
@@ -93,7 +121,7 @@ namespace Escape_room
                         temp[2] = UI.Split(' ')[2];
 
                     }
-                    
+
 
                     if (szokozszam == 0)
                     {
@@ -102,48 +130,143 @@ namespace Escape_room
                             case "menj":
                                 {
                                     Console.WriteLine("Nem adtad meg hová szeretnél menni");
-                                    
+
                                     break;
                                 }
                             case "nézd":
                                 {
-                                    nezd();
+                                    nézd();
                                     break;
 
                                 }
-                                
+
                             case "nyisd":
                                 {
                                     Console.WriteLine("Nem adtad meg mit szeretnél kinyitni");
-                                   
+
                                     break;
                                 }
 
                             case "húzd":
                                 {
                                     Console.WriteLine("Nem adtad meg mit szeretnél húzni");
-                                    
+
                                     break;
                                 }
 
                             case "törd":
                                 {
                                     Console.WriteLine("Nem adtad meg mit szeretnél törni");
-                                    
+
+                                    break;
+                                }
+                            case "help":
+                                {
+                                    help();
+                                    break;
+                                }
+                            case "észak":
+                                {
+                                    észak();
+                                    break;
+                                }
+                            case "dél":
+                                {
+                                    dél();
+                                    break;
+                                }
+                            case "kelet":
+                                {
+                                    kelet();
+                                    break;
+                                }
+                            case "nyugat":
+                                {
+                                    nyugat();
                                     break;
                                 }
                             default:
                                 {
                                     Console.WriteLine("Helytelen parancsot adtál meg");
-                                    
+
                                     break;
                                 }
 
                         }
                     }
 
+                    //if (szokozszam == 1)
+                    //{
+                    //    switch (temp[0])
+                    //    {
+                    //        case "menj":
+                    //            {
+                    //                Console.WriteLine("Nem adtad meg hová szeretnél menni");
 
-                    
+                    //                break;
+                    //            }
+                    //        case "nézd":
+                    //            {
+                    //                nézd();
+                    //                break;
+
+                    //            }
+
+                    //        case "nyisd":
+                    //            {
+                    //                Console.WriteLine("Nem adtad meg mit szeretnél kinyitni");
+
+                    //                break;
+                    //            }
+
+                    //        case "húzd":
+                    //            {
+                    //                Console.WriteLine("Nem adtad meg mit szeretnél húzni");
+
+                    //                break;
+                    //            }
+
+                    //        case "törd":
+                    //            {
+                    //                Console.WriteLine("Nem adtad meg mit szeretnél törni");
+
+                    //                break;
+                    //            }
+                    //        case "help":
+                    //            {
+                    //                help();
+                    //                break;
+                    //            }
+                    //        case "észak":
+                    //            {
+                    //                észak();
+                    //                break;
+                    //            }
+                    //        case "dél":
+                    //            {
+                    //                dél();
+                    //                break;
+                    //            }
+                    //        case "kelet":
+                    //            {
+                    //                kelet();
+                    //                break;
+                    //            }
+                    //        case "nyugat":
+                    //            {
+                    //                nyugat();
+                    //                break;
+                    //            }
+                    //        default:
+                    //            {
+                    //                Console.WriteLine("Helytelen parancsot adtál meg");
+
+                    //                break;
+                    //            }
+                    //    }
+
+
+
                 }
             } while (uiError);
 
@@ -170,29 +293,14 @@ namespace Escape_room
             Console.ReadLine();
             Console.Clear();
         }
-
         public static void intro()
         {
-            String[] introData = new string[] { "Üdvözöllek az Escape Room című kalandjátékban", "A játék célja magától értetődő", "Juss ki a szobából", "A játékban használható parancsok: ", "- nézd", "- vedd fel","- tedd le","- nyisd","- húzd","- törd", "Ezek után kell írnod a tárgy vagy helyszín nevét amellyel a cselekvést végzed", "Különböző irányokba mehetsz, ezekhez használd a 'észak,dél,kelet,nyugat' parancsokat", "Hogy megtekintsd a leltáradban lévő dolgaid, használd a 'leltár' parancsot", "A játékot a 'mentés *fájlnév*' paranccsal tudod elmenteni", "A játékot betölteni korábbi mentésről a 'betöltés *fájlnév*' paranccsal éred el", "Meg is vagyunk, kezdhetjük?", "A játék indításához nyomj 'ENTER'-t" };
+            String[] introData = new string[] { "Üdvözöllek az Escape Room című kalandjátékban", "A játék célja magától értetődő", "Juss ki a szobából", "A játékban használható parancsok: ","- menj", "- nézd", "- vedd fel", "- tedd le", "- nyisd", "- húzd", "- törd", "Ezek után kell írnod a tárgy vagy helyszín nevét amellyel a cselekvést végzed", "Különböző irányokba nézelődhetsz, ezekhez használd a 'észak,dél,kelet,nyugat' parancsokat", "Hogy megtekintsd a leltáradban lévő dolgaid, használd a 'leltár' parancsot", "A játékot a 'mentés *fájlnév*' paranccsal tudod elmenteni", "A játékot betölteni korábbi mentésről a 'betöltés *fájlnév*' paranccsal éred el","'help' paranccsal tudod megnézni a parancsokat", "Meg is vagyunk, kezdhetjük?", "A játék indításához nyomj 'ENTER'-t" };
 
             writeFancy(introData);
             Console.ReadKey();
             Console.Clear();
         }
-
-        public static void nezd()
-        {
-            global global = new global();
-            if (global.currentPlace == 1)
-            {
-                global.nappaliInfo();
-            }
-            if (global.currentPlace == -1)
-            {
-                global.furdoszobaInfo();
-            }
-        }
-
         public static void writeFancy(string[] data)
         {
 
@@ -208,6 +316,99 @@ namespace Escape_room
                 Console.WriteLine();
             }
         }
+        public static void writeFancy(string data)
+        {
+                foreach (char x in data)
+                {
+                    Console.Write(x);
+                    Thread.Sleep(55);
+
+                }
+                Thread.Sleep(200);
+                Console.WriteLine();
+        }
+        
+        public static void nézd()
+        {
+            global global = new global();
+            if (global.currentPlace == 1)
+            {
+                global.nappaliInfo();
+            }
+            if (global.currentPlace == -1)
+            {
+                global.furdoszobaInfo();
+            }
+        }
+        public static void help()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Parancsok: ");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("---észak, dél, kelet, nyugat----");
+            Console.WriteLine("---nézd----");
+            Console.WriteLine("---nézd-------- *tárgy neve*");
+            Console.WriteLine("---vedd fel---- *tárgy neve*");
+            Console.WriteLine("---tedd le----- *tárgy neve*");
+            Console.WriteLine("---nyisd------- *tárgy neve*");
+            Console.WriteLine("---húzd-------- *tárgy neve*");
+            Console.WriteLine("---törd-------- *tárgy neve*");
+            Console.WriteLine("---leltár------");
+            Console.WriteLine("---mentés------ *file neve*");
+            Console.WriteLine("---betöltés---- *file neve*");
+            Console.WriteLine("---help--------");
+            Console.ResetColor();
+        }
+        public static void észak()
+        {
+            global global = new global();
+            if (global.currentPlace == 1)
+            {
+
+                writeFancy("Északra egy szekrény található.");
+            }
+            else if (global.currentPlace == -1)
+            {
+                writeFancy("Északra nincs semmi.");
+            }
+        }
+        public static void dél()
+        {
+            global global = new global();
+            if (global.currentPlace == 1)
+            {
+                writeFancy("Délre nincs semmi");
+            }
+            else if (global.currentPlace == -1)
+            {
+                writeFancy("Délre nincs semmi.");
+            }
+
+        }
+        public static void kelet()
+        {
+            global global = new global();
+            if (global.currentPlace == 1)
+            {
+                writeFancy("Keletre nincs semmi.");
+            }
+            else if (global.currentPlace == -1)
+            {
+                writeFancy("Keletre a nappali van.");
+            }
+        }
+        public static void nyugat()
+        {
+            global global = new global();
+            if (global.currentPlace == 1)
+            {
+                writeFancy("Nyugatra egy ajtó található");
+            }
+            else if (global.currentPlace == -1)
+            {
+                writeFancy("Nyugatra egy kád található");
+            }
+        }
 
 
     }
@@ -218,7 +419,8 @@ namespace Escape_room
        
         public bool nyitva = false;
         public bool eltolva = false;
-        
+        public bool ottvagy = false;
+
 
         public void nyisd()
         {
@@ -258,6 +460,7 @@ namespace Escape_room
     class ajto
     {
         public bool nyitva = false;
+        public bool ottvagy = false;
 
         public void nyisd()
         {
@@ -284,6 +487,11 @@ namespace Escape_room
         {
             hely = currentPlace;
         }
+    }
+    class kad
+    {
+        public bool ottvagy = false;
+        public bool megnezve = false;
     }
 }
 
